17abaf827b4f32146589e696cc62e11e70393d08
Albert González <albert@mailtrack.io>
2020-03-11 18:22:27 +0100

    this exercise solved

--- examples/this.exercise.js
+++ examples/this.exercise.js
@@ -1,5 +1,59 @@
 export const ADD = 'add';
 
 export default function() {
-    return {};
+    return {
+        screen: '',
+        currentOperation: null,
+        storedNumber: null,
+        resetScreenOnPushNumber: false,
+
+        pushNumber(number) {
+            if (isNaN(number)) {
+                throw new Error('Not a number');
+            }
+
+            const stringNumber = number + '';
+
+            if (stringNumber.length > 1) {
+                throw new Error('Number can not be greater than 9');
+            }
+
+            if (this.resetScreenOnPushNumber) {
+                this.screen = '';
+                this.resetScreenOnPushNumber = false;
+            }
+
+            if (this.screen.length === 6) {
+                return;
+            }
+
+            this.screen += number;
+        },
+
+        pushAdd() {
+            this.currentOperation = ADD;
+            this.resetScreenOnPushNumber = true;
+
+            if (this.storedNumber !== null) {
+                this.storedNumber =
+                    this.storedNumber + parseInt(this.screen, 10);
+                this.screen = this.storedNumber + '';
+
+                return;
+            }
+
+            this.storedNumber = parseInt(this.screen, 10);
+        },
+
+        pushShowResult() {
+            if (this.currentOperation === ADD) {
+                this.screen =
+                    this.storedNumber + parseInt(this.screen, 10) + '';
+                this.storedNumber = null;
+                this.resetScreenOnPushNumber = true;
+            }
+
+            this.currentOperation = null;
+        },
+    };
 }
